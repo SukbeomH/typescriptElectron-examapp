d912371f2ec151626512f16ee349b23bfdcb5ece
SukbeomH <brent93.dev@gmail.com>
2023-07-05 15:26:17 +0900

    [Feature] electron Refactoring

--- src/module/updater.ts
+++ src/module/updater.ts
@@ -3,17 +3,23 @@ import { UpdateCheckResult, autoUpdater } from "electron-updater";
 
 export const updateManager = async (): Promise<boolean> => {
   const updateStatus: UpdateCheckResult = await autoUpdater.checkForUpdates();
-  dialog.showMessageBox({
-    type: "info",
-    title: "업데이트 확인",
-    message: `${updateStatus.updateInfo.version} 버전이 확인되었습니다.\n
+  if (updateStatus) {
+    dialog.showMessageBox({
+      type: "info",
+      title: "업데이트 확인",
+      message: `${updateStatus.updateInfo.version} 버전이 확인되었습니다.\n
     ${updateStatus.updateInfo}`,
-    buttons: ["확인"],
-  }).then(({response}) => {
-    if (response === 0) {
-      autoUpdater.quitAndInstall();
-    }
-    return false;
-  })
+      buttons: ["확인"],
+    }).then(({ response }) => {
+      if (response === 0) {
+        autoUpdater.quitAndInstall();
+      }
+      return false;
+    })
+  }
   return true;
 }
+
+// autoUpdater.on("update-available", () => {
+//   autoUpdater.downloadUpdate();
+// });
